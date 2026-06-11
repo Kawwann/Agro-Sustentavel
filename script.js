b9e9b10b4c98e044b1ec8fe44f70370a74e45750
Kawann <kawan.romao.costa@escola.pr.gov.br>
2026-06-11 12:28:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Aqui podemos adicionar interatividade simples
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('🌿 Obrigado por se interessar pelo agro sustentável!');
+    });
+});
